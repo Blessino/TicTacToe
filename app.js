53a10ea06f337748946ef064efb2e1d5a6abc9bc
Ashish <grunch.me1@gmail.com>
2024-01-31 21:20:22 +0545

    square constructor created

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// A square represents a square on the board.
+// It contains position, size information and,
+// optionally, an actor that should be drawn on it.
+class Square{
+    constructor(x, y, width, height, ctx) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height =  height;
+        this.ctx = ctx;
+        this.actor = null;
+    }
+
+    // The draw method is used to draw the square.
+    draw() {
+        //
+    }
+}
